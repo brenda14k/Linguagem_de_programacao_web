df5f5fd40b523253195a3c1ee8f95938bc607add
brenda14k <brendam.ifpr2024@gmail.com>
2026-03-18 11:31:22 -0300

    Create index.php

---
+++ jogo_original/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Jogo da Adivinhação</title>
+</head>
+<body>
+
+<h2>Escolha um palpite:</h2>
+
+<a href="jogo.php?palpite=1">Palpite 1</a><br>
+<a href="jogo.php?palpite=2">Palpite 2</a><br>
+<a href="jogo.php?palpite=3">Palpite 3</a><br>
+<a href="jogo.php?palpite=4">Palpite 4</a><br>
+<a href="jogo.php?palpite=5">Palpite 5</a><br>
+<a href="jogo.php?palpite=6">Palpite 6</a><br>
+<a href="jogo.php?palpite=7">Palpite 7</a><br>
+<a href="jogo.php?palpite=8">Palpite 8</a><br>
+<a href="jogo.php?palpite=9">Palpite 9</a><br>
+<a href="jogo.php?palpite=10">Palpite 10</a><br>
+
+</body>
+</html>
